120c1a2c23719b993b58e4dc64cfe8e4529bb89b
VladimirSivac <morndes@gmail.com>
2024-10-29 10:57:57 +0300

    Project_2
    Компьютерный практикум 6

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            /*Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.ReadLine();*/
+
+            // Task 2
+            /*Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{a} + {b} = {b} + {a}");
+            Console.ReadLine();*/
+
+            // Task 3
+            /*Console.Write("a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a:f1} * {b:f1} = {a * b : f1}");
+            Console.ReadLine();*/
+
+            // Task 4
+            /*Console.Write("a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("c= ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"({a:f2} + {b:f2}) + {c:f2} = {a:f2} + ({b:f2} + {c:f2})");
+            Console.ReadLine();*/
+
+            // Task 5
+            Console.Write("a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("c= ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.Write("d= ");
+            double d = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a:f2} / {b:f2} + {c:f2} / {d:f2} = {a:f2 / b:f2 + c:f2 / d:f2}");
+            Console.ReadLine();
+        }
+    }
+}
