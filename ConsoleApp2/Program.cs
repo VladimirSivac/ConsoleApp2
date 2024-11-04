a6051f034b7fcf747a888b21cc4a3ee4ce30a4da
VladimirSivac <morndes@gmail.com>
2024-11-04 18:58:01 +0200

    10 tasks done
    Выполненный Компьютерный практикум № 6 (10 задач)

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -37,7 +37,7 @@ namespace ConsoleApp2
             Console.Write("b= ");
             double b = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{a:f1} * {b:f1} = {a * b : f1}");
+            Console.WriteLine($"{a:f1} * {b:f1} = {(a * b):f1}");
             Console.ReadLine();*/
 
             // Task 4
@@ -54,7 +54,7 @@ namespace ConsoleApp2
             Console.ReadLine();*/
 
             // Task 5
-            Console.Write("a= ");
+            /*Console.Write("a= ");
             double a = double.Parse(Console.ReadLine());
 
             Console.Write("b= ");
@@ -66,8 +66,70 @@ namespace ConsoleApp2
             Console.Write("d= ");
             double d = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{a:f2} / {b:f2} + {c:f2} / {d:f2} = {a:f2 / b:f2 + c:f2 / d:f2}");
-            Console.ReadLine();
+            Console.WriteLine($"{a:f2} / {b:f2} + {c:f2} / {d:f2} = {(a / b + c / d):f2}");
+            Console.ReadLine();*/
+
+            // Task 6
+            /*Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"a + b = {a} + {b} = {a + b}  ");
+
+            Console.Write($"a - b = {a} - {b} = {a - b}  ");
+
+            Console.Write($"a * b = {a} * {b} = {a * b}");
+            Console.ReadLine();*/
+
+            // Task 7
+            /*Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("c= ");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"[a * (b + c)] = [(b + c) * a]:  [{a} * ({b} + {c})] = [({b} + {c}) * {a}] = {a * (b + c)}");
+            Console.ReadLine();*/
+
+            // Task 8
+            /*Console.Write("a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("c= ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"({a:f4} + ({b:f4} + {c:f4})) = ({a:f4} + {c:f4} + {b:f4})");
+            Console.ReadLine();*/
+
+            // Task 9
+            /*Console.Write("x= ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("y= ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Округленный с точностью до 4 знаков результат:  {x * 3} / {y} = {(x * 3 / y):f4}");
+            Console.ReadLine();*/
+
+            // Task 10
+            /*Console.Write("a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write($"a * b = {a:f3} * {b:f3} = {(a * b):f3}  ");
+
+            Console.Write($"a * b = {a:f5} * {b:f5} = {(a * b):f5}");
+            Console.ReadLine();*/
         }
     }
 }
